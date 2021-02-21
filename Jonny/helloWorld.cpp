9657f457707d89eb7685042ee50b187fb79a336e
JKeane4210 <64436387+JKeane4210@users.noreply.github.com>
2021-02-21 09:43:33 -0600

    My template so far

--- Jonny/helloWorld.cpp
+++ Jonny/helloWorld.cpp
@@ -1,12 +1,27 @@
 #include <bits/stdc++.h>
+#define nl "\n"
+
 using namespace std;
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
 
+    int a[20];
+    cout << "Size of int array: " << sizeof(a) / sizeof(int) << endl;
+    std::memset(a, 0, sizeof a);
+    cout << "Memset: ";
+    for (int i = 0; i < 20; ++i) {
+        cout << a[i] <<  " ";
+    }
+    cout << endl << "String Length: " << strlen("hello") << endl;
+
     // code here
+    string s = "hi";
     cout << "Hello World!" << endl;
+    cout << "String Length 2: " << s.length() << endl;
+    // getchar() gives me a character from input
+    cout << "a" << nl << "b" << nl;
 
     return 0;
 }
---
+++ Jonny/template.cpp
@@ -0,0 +1,19 @@
+//
+// Created by Jonny Keane on 2/21/21.
+//
+
+#include <bits/stdc++.h>
+
+#define nl "\n"
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    // code here
+
+    return 0;
+}
+
