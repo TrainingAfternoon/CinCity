d0ccdefac4959eb0e91c39ea024975615470de0d
JKeane4210 <64436387+JKeane4210@users.noreply.github.com>
2021-02-26 22:25:22 -0600

    Taking some out of Mitchell's template for mine

--- Jonny/template.cpp
+++ Jonny/template.cpp
@@ -4,7 +4,28 @@
 
 #include <bits/stdc++.h>
 
-#define nl "\n"
+
+#define ll long long
+#define ld long double
+#define pb push_back
+
+#define V vector
+#define vi V<int>
+#define vll V<ll>
+#define vd V<double>
+#define pii pair<int, int>
+#define pll pair<ll,ll>
+#define vpii V<pii>
+#define vpll V<pll>
+#define graph V<vi>
+
+// loops
+#define WHILE(n) while(n--)
+#define FOR(a) for(ll i=0;i<a;i++)
+#define minimum(a) *min_element(a.begin(), a.end())
+#define maximum(a) *max_element(a.begin(), a.end())
+
+#define nl '\n'
 
 using namespace std;
 
