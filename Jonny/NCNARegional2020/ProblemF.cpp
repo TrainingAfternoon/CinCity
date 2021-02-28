682ba3c365704389808edea4778e174da32cc6b8
JKeane4210 <64436387+JKeane4210@users.noreply.github.com>
2021-02-28 10:52:05 -0600

    Problem F passing sample input but getting wrong answer for actual run?

--- Jonny/NCNARegional2020/ProblemF.cpp
+++ Jonny/NCNARegional2020/ProblemF.cpp
@@ -23,6 +23,7 @@
 #define FOR(a) for(ll i=0;i<a;i++)
 #define minimum(a) *min_element(a.begin(), a.end())
 #define maximum(a) *max_element(a.begin(), a.end())
+#define FIND(a, e) find(a.begin(), a.end(), e)
 
 #define nl '\n'
 
@@ -37,7 +38,30 @@ public:
 };
 
 bool operator<(const DijstrasStep& a, const DijstrasStep& b) {
-    return a.totalDist > b.totalDist;
+    return a.totalDist < b.totalDist;
+}
+
+pair<int, int> DFS(int curr, vector<DijstrasStep> *graph_) {
+    vi visited;
+    visited.pb(curr);
+    int sum = 0;
+    while (true) {
+        int count = 0;
+        priority_queue<DijstrasStep> pq;
+        for (DijstrasStep d: graph_[curr]) {
+            if (FIND(visited, d.nodeID) == visited.end()) {
+                pq.push(d);
+//                cout << d.nodeID << ", " << d.totalDist << nl;
+                count++;
+            }
+        }
+        if (count == 0) break;
+        curr = pq.top().nodeID;
+        visited.pb(curr);
+        sum += pq.top().totalDist;
+//        cout << "\tS: " << sum << nl;
+    }
+    return make_pair(curr, sum);
 }
 
 int main() {
@@ -56,28 +80,14 @@ int main() {
         nodeA--;
         nodeB--;
         sum += weight;
-        graph_[nodeA].pb(DijstrasStep(nodeB, weight)); //only one add because it is directional
+        graph_[nodeA].pb(DijstrasStep(nodeB, weight));
         graph_[nodeB].pb(DijstrasStep(nodeA, weight));
     }
     if (k >= 2) {
         cout << sum;
     } else {
-        vi visited;
-        visited.pb(0);
-        int curr = 0;
-        while (true) {
-            sort(graph_[curr].begin(), graph_[curr].end());
-            int j;
-//            while(find(visited.begin(), visited.end(), graph_[j])) {
-//                if ()
-//            }
-//            visited.pb(d.nodeID);
-//            curr = d.nodeID;
-        }
-        cout << curr;
+        cout << DFS(DFS(0, graph_).first, graph_).second;
     }
-
-    // code here
 
     return 0;
 }
