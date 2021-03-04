40f4e03b52fdedb4cb16845f1608cd583f75123a
JKeane4210 <64436387+JKeane4210@users.noreply.github.com>
2021-03-04 14:22:20 -0600

    ProblemJ working with test inputs but not getting accepted

--- Jonny/NCNARegional2020/ProblemJ.cpp
+++ Jonny/NCNARegional2020/ProblemJ.cpp
@@ -33,45 +33,42 @@ struct LaceStep {
     int currNode;
 };
 
-vector<pair<int, int>> create_possibilities(int N, int d, int s, int t, int fmin, int fmax) {
-    vector<pair<int, int>> res;
+vector<pair<double, double>> create_possibilities(int N, int d, int s, int t, int fmin, int fmax) {
+    vector<pair<double, double>> res;
     bool eyelets[2 * (N - 1)]; // excluding the first row because always the same
     stack<LaceStep> stack_;
     LaceStep curr = LaceStep();
     curr.currEyelets = eyelets;
     curr.totalLength = s; // I want do the whole distance
-    curr.currNode = -1;
+    curr.currNode = -2;
     stack_.push(curr);
     while (!stack_.empty()) {
         curr = stack_.top();
         stack_.pop();
         if (curr.currNode == 2 * (N - 1) - 1 || curr.currNode == 2 * (N - 1) - 2) {
-            res.push_back(make_pair(curr.totalLength + fmin, curr.totalLength + fmax));
+            double tDistance = 2 * t;
+            for (int i = 0; i < 2 * (N - 1); ++i) {
+                if (curr.currEyelets[i]) tDistance += t;
+            }
+            res.emplace_back(make_pair(curr.totalLength + tDistance + 2 * fmin, curr.totalLength + tDistance + 2 * fmax));
             continue;
         }
         for (int i = 0; i < 2 * (N - 1); ++i) {
             if (!curr.currEyelets[i] && curr.currNode != i) {
                 LaceStep step = LaceStep();
-                bool new_eyelets[2 * (N - 1)];
+                bool *new_eyelets = new bool[2 * (N - 1)];
                 copy(curr.currEyelets, curr.currEyelets + 2 * (N - 1), new_eyelets + 0);
-                new_eyelets[i] = true;
-                if (i % 2 == 0) {
-                    new_eyelets[i + 1] = true;
-                } else  {
-                    new_eyelets[i - 1] = true;
-                }
+                new_eyelets[2 * (i / 2)] = true;
+                new_eyelets[2 * (i / 2) + 1] = true;
                 step.currEyelets = new_eyelets;
                 step.currNode = i;
-                // step.totalLength = some implementation for distancep
-                if (curr.currNode == -1) {
-                    step.totalLength = i % 2 == 0 ? 2 * (i + 1) * d + curr.totalLength :
-                            2 * sqrt(pow((double)((i + 1) * d), 2) + pow((double)(s + t), 2)) + curr.totalLength;
-                } else {
-                    int separation = abs(d - i);
-                    step.totalLength = separation % 2 == 0 ? 2 * separation * d + curr.totalLength :
-                            2 * sqrt(pow((double)(separation * d), 2) + pow((double)(s + t), 2)) + curr.totalLength;
+                int separation = abs(curr.currNode - i);
+                int changeLevel = abs (curr.currNode / 2 - i /2);
+                if (separation % 2 == 1 || changeLevel == 1) {
+                    step.totalLength = separation % 2 == 0 ? 2 * changeLevel * d + curr.totalLength :
+                                       2 * sqrt(pow(changeLevel * d, 2) + pow(s, 2)) + curr.totalLength;
+                    stack_.push(step);
                 }
-                stack_.push(step);
             }
         }
     }
@@ -81,14 +78,12 @@ vector<pair<int, int>> create_possibilities(int N, int d, int s, int t, int fmin
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
-    // code here
     int N,d,s,t,fmin,fmax;
     cin >> N >> d >> s >> t >> fmin >> fmax;
-    vector<pair<int, int>> possible_ranges = create_possibilities(N, d, s, t, fmin, fmax);
-    for (auto a: possible_ranges)
-    {
-        cout << a.first << " " << a.second << nl;
-    }
+    vector<pair<double, double>> possible_ranges = create_possibilities(N, d, s, t, fmin, fmax);
+//    for (auto a: possible_ranges) {
+//        cout << a.first << " " << a.second << nl;
+//    }
     int L;
     while (cin >> L) {
         int count = 0;
