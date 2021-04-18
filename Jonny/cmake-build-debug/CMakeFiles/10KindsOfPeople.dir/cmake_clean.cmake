94a8d75f3ef8441057b6698bbe46a5c55da468ee
JKeane4210 <jtkeane8@gmail.com>
2021-04-18 03:38:25 +0000

    Practice NARC

--- Jonny/cmake-build-debug/CMakeFiles/10KindsOfPeople.dir/cmake_clean.cmake
+++ Jonny/cmake-build-debug/CMakeFiles/10KindsOfPeople.dir/cmake_clean.cmake
@@ -2,6 +2,13 @@ file(REMOVE_RECURSE
   "10KindsOfPeople"
   "10KindsOfPeople.pdb"
   "CMakeFiles/10KindsOfPeople.dir/KattisProblems/10KindsOfPeople.cpp.o"
+  "CMakeFiles/10KindsOfPeople.dir/PracticeNADC/problemA.cpp.o"
+  "CMakeFiles/10KindsOfPeople.dir/PracticeNADC/problemB.cpp.o"
+  "CMakeFiles/10KindsOfPeople.dir/PracticeNADC/problemE.cpp.o"
+  "CMakeFiles/10KindsOfPeople.dir/PracticeNADC/problemF.cpp.o"
+  "CMakeFiles/10KindsOfPeople.dir/PracticeNADC/problemG.cpp.o"
+  "CMakeFiles/10KindsOfPeople.dir/PracticeNADC/problemI.cpp.o"
+  "CMakeFiles/10KindsOfPeople.dir/PracticeNADC/problemK.cpp.o"
 )
 
 # Per-language clean rules from dependency scanning.
--- Jonny/cmake-build-debug/CMakeFiles/Euler.dir/DependInfo.cmake
+++ Jonny/cmake-build-debug/CMakeFiles/Euler.dir/DependInfo.cmake
@@ -4,9 +4,9 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/Users/jkeane/CLionProjects/CinCity/Jonny/PracticeNCNA/eulers.cpp" "/Users/jkeane/CLionProjects/CinCity/Jonny/cmake-build-debug/CMakeFiles/Euler.dir/PracticeNCNA/eulers.cpp.o"
+  "/home/team/workspace/CinCity/Jonny/PracticeNCNA/eulers.cpp" "/home/team/workspace/CinCity/Jonny/cmake-build-debug/CMakeFiles/Euler.dir/PracticeNCNA/eulers.cpp.o"
   )
-set(CMAKE_CXX_COMPILER_ID "AppleClang")
+set(CMAKE_CXX_COMPILER_ID "GNU")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
