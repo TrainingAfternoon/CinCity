13b286fa8f2b24af2045ed7959b18f33a680570a
JKeane4210 <64436387+JKeane4210@users.noreply.github.com>
2021-05-02 09:13:46 -0500

    Read array function seems to work but code jam did not like it, so I would avoid using

--- LibraryCode/ArrayInput.cpp
+++ LibraryCode/ArrayInput.cpp
@@ -7,12 +7,13 @@
  * Reads an array of generic elements
  * Pre: The type of the elements should be capable of being input through cin calls
  * @tparam T The type of the objects in the return array
+ * @param arr Pointer to the array to fill
  * @param size The size of the array
  * @return A pointer to the array of generic elements
  */
+ // Code Jam gave this a runtime error
 template <typename T>
-T* readArray(int size) {
-    T* arr = new T(size);
+T* readArray(T* arr, int size) {
     for (int i = 0; i < size; ++i) {
         cin >> arr[i];
     }
