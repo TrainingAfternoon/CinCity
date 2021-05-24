518b4a622b71eaefd777ccb02739281e5a4e6182
JKeane4210 <64436387+JKeane4210@users.noreply.github.com>
2021-05-23 23:16:31 -0500

    Work so far on Week 1 [Don't Look If Want To Solve On Own]

---
+++ Jonny/cmake-build-debug/CMakeFiles/PizzaCutting.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PizzaCutting.dir/MSOE2021Summer/Week1/PizzaCutting.cpp.o"
+  "PizzaCutting"
+  "PizzaCutting.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PizzaCutting.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
