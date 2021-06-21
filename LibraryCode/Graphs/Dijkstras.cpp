1a790ecec2ad0c6690c92d8ae97ea74121af0128
JKeane4210 <64436387+JKeane4210@users.noreply.github.com>
2021-06-20 23:44:34 -0500

    Quick fix to Dijkstra's namespace

--- LibraryCode/Graphs/Dijkstras.cpp
+++ LibraryCode/Graphs/Dijkstras.cpp
@@ -26,39 +26,31 @@ namespace DijkstrasAlg {
         /// \param a A step
         /// \param b Another step
         /// \return If a is "lesser"
-        friend bool operator<(const DijkstrasStep<W> &a, const DijkstrasStep<W> &b);
+        /// \tparam The type of the weight (int/double)
+        friend bool operator<(const DijkstrasStep<W> &a, const DijkstrasStep<W> &b) {
+            return a.totalDist > b.totalDist;
+        }
     };
-
-    /// Operator for comparing the Dij
-    /// \param a A step
-    /// \param b Another step
-    /// \return If a is "lesser"
-    /// \tparam The type of the weight (int/double)
-    template<typename W>
-    bool operator<(const DijkstrasStep<W> &a, const DijkstrasStep<W> &b) {
-        return a.totalDist > b.totalDist;
-    }
 
     /// Class to hold Dijsktra's Algorithm
     /// \tparam W The type of the weight (int/double)
     template<typename W>
     class Dijkstras {
     public:
-        vector<pair<int, W>> graph;
+        vector<pair<int, W>> graph*;
         W* shortestPathTable;
+        int numNodes;
 
         /// Initializes the Dijkstras object
         /// \param graph The assembled graph
-        Dijkstras(vector<pair<int, W>> graph) : graph(graph) {
-            shortestPathTable = new W[graph.size()];
-            for (int i = 0; i < graph.size(); ++i) {
-                shortestPathTable[i] = INF;
-            }
+        Dijkstras(vector<pair<int, W>> graph*, int numNodes) : graph(graph), numNodes(numNodes) {
+            shortestPathTable = new W[numNodes];
         }
 
         /// Runs Dijkstra's Algorithm on the graph
         /// \param start The node to start at
         void dijkstras(int start) {
+            clearShortestPaths();
             priority_queue<DijkstrasStep<W>> pq;
             pq.push(DijkstrasStep<W>(start, 0));
             //MAIN LOOP
@@ -72,6 +64,14 @@ namespace DijkstrasAlg {
                 for (const auto& p: graph[shortest.nodeID]) {
                     pq.push(DijkstrasStep<W>(p.first, shortest.totalDist + p.second));
                 }
+            }
+        }
+
+    private:
+        /// Clears the shortestPathTable for another run of algorithm from another starting point
+        void clearShortestPaths() {
+            for (int i = 0; i < graph.size(); ++i) {
+                shortestPathTable[i] = INF;
             }
         }
     };
